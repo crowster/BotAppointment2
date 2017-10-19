363c03c4c19dfc68bfcd1a3ce68f28115dda2060
Jesus Gonzalez <jesus.martinez@acftechnologies.com>
2017-10-19 16:39:02 -0500

    start project

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=BotAppointment-Web-Deploy.pubxml -p:Password=rFtHRpsntaXc7uBAmfZaCM4iEwWirwapXbexa4WBXcX7bosMgDagQQfzkZ1b
+
